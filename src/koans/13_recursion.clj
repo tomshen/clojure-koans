a514bbd22823304afa283dc1d4b0642ba21fbcc9
Tom Shen <tom@tomshen.me>
2013-07-24 22:53:04 -0400

    Clojure enlightenment

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,26 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= (count coll) 1)
+    coll
+    (cons (peek coll) (recursive-reverse (pop coll)))))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+  (if (= n 1)
+    acc
+    (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -29,7 +35,7 @@
   "Reversing directions is easy when you have not gone far"
   (= '(1) (recursive-reverse [1]))
 
-  "Yet it becomes more difficult the more steps you take"
+  "Yet more difficult the more steps you take"
   (= '(5 4 3 2 1) (recursive-reverse [1 2 3 4 5]))
 
   "Simple things may appear simple."
